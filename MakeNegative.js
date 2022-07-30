6965c534a41c1acfac899ec19e775bf42b2b03f7
stefan <stefanbayne@gmail.com>
2022-07-29 20:23:59 -0400

    Make Negative if positive.

--- MakeNegative.js
+++ MakeNegative.js
@@ -4,6 +4,10 @@
 
 // We're going to use a ternary operator that checks if the number is negative.
 // if it is, return the number, else return the negative number.
+
+/*
+In JavaScript, to make a variable negative, we can just put a negative sign in front.
+*/
 const makeNegative = number => {
     return (number < 0) ? number: -number
 }
