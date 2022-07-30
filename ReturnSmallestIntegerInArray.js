92b15b25d597dbb1622c4dd37d6448f013895cf4
stefan <stefanbayne@gmail.com>
2022-07-29 20:54:02 -0400

    Return Smallest Integer.

---
+++ ReturnSmallestIntegerInArray.js
@@ -0,0 +1,16 @@
+/**
+ * This is how we would return the smallest integer in an array.
+ */
+
+// given an array of integers as an argument:
+
+const returnSmallestInt = args => {
+    return Math.min(args)
+}
+
+let args = [2, 5, 8, 10, 14]
+console.log(returnSmallestInt(args))
+
+/**
+ * assuming the argument passed in is an array of values.
+ */
