134b0d4cac770028ff1c2fb0392e179f0a9991bc
stefan <stefanbayne@gmail.com>
2022-08-01 21:10:32 -0400

    Isograms challenge.

---
+++ Isograms.js
@@ -0,0 +1,14 @@
+/**
+ * Check to see if a string has repeating letters.
+ */
+function isIsogram(str) {
+    var i, j;
+    str = str.toLowerCase();
+    for(i = 0; i < str.length; ++i)
+      for(j = i + 1; j < str.length; ++j)
+        if(str[i] === str[j])
+          return false;
+    return true;
+}
+
+console.log(isIsogram("Here I am."))
